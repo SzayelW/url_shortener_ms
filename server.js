11c473480698d906c811dafa90e268b311e4f484
Andres <andres70p32@gmail.com>
2017-04-10 23:09:06 +0000

    basic structure

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-var mongodb = require('mongodb').MongoClient
-var url = ""
+//var mongodb = require('mongodb').MongoClient
+var express = require('express')
+var mongoose = require('mongoose')
+var schema = mongoose.schema
+var app = express()
+var validURL = require('valid-url')
+var url = process.env.MONGOLAB_URI
 
-mongodb.connect(url, (err, db)=>{
-    if(err){
-        console.error('Database error' + err)
+app.get('/', (req, res)=>{
+    res.send('Homepage')
+})
+
+app.get('/:shortUrl', (req, res)=>{
+    var data = req.params.shortUrl
+    if(!isNaN(data)){
+        res.send(data)        
     }else{
-        console.log('Database connected')
-        
-        db.close()
+      res.send("invalid data")   
     }
-    
-});
+})
+
+app.get('/new/*', (req, res)=>{
+    var url = req.param(0)
+    if(validURL.isWebUri(url)){
+        var randomUrl = Math.floor((Math.random()*9999)+1)
+        var newUrl = req.headers['x-forwarded-proto']+"://"+req.headers['host']+"/"+randomUrl
+        res.json({originalUrl: url, shortUrl: newUrl })
+    }else{
+        res.json({error: "invalid url"})
+    }
+})
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("listening on port: " + process.env.PORT || "8080")
+})
