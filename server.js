b9e848d20a31b1d948684c79e75c1132210658d5
Andres <andres70p32@gmail.com>
2017-04-11 00:35:03 +0000

    first version, ready to deploy

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-//var mongodb = require('mongodb').MongoClient
+var mongodb = require('mongodb').MongoClient
 var express = require('express')
-var mongoose = require('mongoose')
-var schema = mongoose.schema
 var app = express()
 var validURL = require('valid-url')
-var url = process.env.MONGOLAB_URI
-
+var bodyParser = require('body-parser')
+var mongoUrl = process.env.MONGOLAB_URI 
+var jsonParser = bodyParser.json()
 app.get('/', (req, res)=>{
     res.send('Homepage')
 })
@@ -13,18 +12,33 @@ app.get('/', (req, res)=>{
 app.get('/:shortUrl', (req, res)=>{
     var data = req.params.shortUrl
     if(!isNaN(data)){
-        res.send(data)        
+        var url = req.headers['x-forwarded-proto']+"://"+req.headers['host']+"/"+data
+        mongodb.connect(mongoUrl, (err,db)=>{
+            if(err){ throw err}
+            db.collection('urls_col').find({short_url : url}).toArray((err, doc)=>{
+                if(err){throw err}
+                res.redirect(doc[0].original_url)
+            })
+        })
     }else{
-      res.send("invalid data")   
+      res.json({error: "invalid data"})   
     }
+    res.json({error : "short url doesnt exist"})
 })
 
-app.get('/new/*', (req, res)=>{
+app.get('/new/*', jsonParser, (req, res)=>{
     var url = req.param(0)
     if(validURL.isWebUri(url)){
         var randomUrl = Math.floor((Math.random()*9999)+1)
         var newUrl = req.headers['x-forwarded-proto']+"://"+req.headers['host']+"/"+randomUrl
-        res.json({originalUrl: url, shortUrl: newUrl })
+        var urlDoc = {original_url : url, short_url : newUrl} 
+        
+        mongodb.connect(mongoUrl, (err, db)=>{
+            if(err){ throw err}
+            db.collection('urls_col').insert(urlDoc)
+            db.close()
+        })
+        res.json(urlDoc)
     }else{
         res.json({error: "invalid url"})
     }
